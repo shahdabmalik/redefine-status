4eecf13b72c99d877a7610496163eddd59b80b6e
shahdabmalik <shahdab888@gmail.com>
2024-03-15 14:42:10 +0530

    footer styles update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,29 +6,29 @@ const Footer = () => {
                 <div>
                     <h4 className='font-semibold text-lg' >PARTNERS</h4>
                     <div className='flex flex-col gap-2 mt-4 text-sm' >
-                        <a href='https://systemicaltruism.com' target='_blank' rel="noreferrer" className='hover:underline' >Systemic Altruism</a>
-                        <a href='https://dia.wiki' target='_blank' rel="noreferrer" className='hover:underline' >Decentralized Intelligence Agency</a>
-                        <a href='https://purplerock.xyz' target='_blank' rel="noreferrer" className='hover:underline' >Purplerock </a>
-                        <a href='http://mooworldorder.org' target='_blank' rel="noreferrer" className='hover:underline' >Moo World Order</a>
-                        <a href='https://secreteservice.com' target='_blank' rel="noreferrer" className='hover:underline' >Secrete Service</a>
+                        <a href='https://systemicaltruism.com' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Systemic Altruism</a>
+                        <a href='https://dia.wiki' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Decentralized Intelligence Agency</a>
+                        <a href='https://purplerock.xyz' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Purplerock </a>
+                        <a href='http://mooworldorder.org' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Moo World Order</a>
+                        <a href='https://secreteservice.com' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Secrete Service</a>
                     </div>
                 </div>
                 <div>
                     <h4 className='font-semibold text-lg' >OPPURTUNITIES</h4>
                     <div className='flex flex-col gap-2 mt-4 text-sm' >
-                        <a href='http://startupcollege.io' target='_blank' rel="noreferrer" className='hover:underline' >Apply To Join Startup College</a>
-                        <a href='https://persistventures.com/investor-application' target='_blank' rel="noreferrer" className='hover:underline' >Invest In Impact Focused Ventures</a>
-                        <a href='https://aiccelerator.org' target='_blank' rel="noreferrer" className='hover:underline' >Join Our Ai Accelerator</a>
-                        <a href='https://effectiveaccelerator.com' target='_blank' rel="noreferrer" className='hover:underline' >Join Our General Accelerator</a>
-                        <a href='https://apps.apple.com/us/app/holy-vible/id6444683138' target='_blank' rel="noreferrer" className='hover:underline' >Download Holy Vible On IOS</a>
-                        <a href='https://play.google.com/store/apps/details?id=com.subverse.vible' target='_blank' rel="noreferrer" className='hover:underline' >Download Holy Vible On Playstore</a>
+                        <a href='http://startupcollege.io' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Apply To Join Startup College</a>
+                        <a href='https://persistventures.com/investor-application' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Invest In Impact Focused Ventures</a>
+                        <a href='https://aiccelerator.org' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Join Our Ai Accelerator</a>
+                        <a href='https://effectiveaccelerator.com' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Join Our General Accelerator</a>
+                        <a href='https://apps.apple.com/us/app/holy-vible/id6444683138' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Download Holy Vible On IOS</a>
+                        <a href='https://play.google.com/store/apps/details?id=com.subverse.vible' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Download Holy Vible On Playstore</a>
                     </div>
                 </div>
                 <div>
                     <h4 className='font-semibold text-lg' >VENTURE ECOSYSTEM:</h4>
                     <div className='flex flex-col gap-2 mt-4 text-sm' >
-                        <a href='https://persistventures.com' target='_blank' rel="noreferrer" className='hover:underline' >Persist Ventures</a>
-                        <a href='https://4rf.org' target='_blank' rel="noreferrer" className='hover:underline' >Fourth Revolution Fund</a>
+                        <a href='https://persistventures.com' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Persist Ventures</a>
+                        <a href='https://4rf.org' target='_blank' rel="noreferrer" className='hover:underline opacity-80 hover:opacity-100' >Fourth Revolution Fund</a>
                     </div>
                 </div>
             </div>
